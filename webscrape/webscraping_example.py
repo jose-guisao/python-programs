26ebb49e7a65eec57ec1fead9e40c9243c65c1ab
Jose Guisao <jguisao@gmail.com>
2022-01-24 20:07:19 -0400

    fix webdriver path

--- webscrape/webscraping_example.py
+++ webscrape/webscraping_example.py
@@ -1,14 +1,16 @@
-from selenium import webdriver 
-from selenium.webdriver.common.by import By 
-from selenium.webdriver.support.ui import WebDriverWait 
-from selenium.webdriver.support import expected_conditions as EC 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 import time
 
 option = webdriver.ChromeOptions()
 option.add_argument(" — incognito")
 
-browser = webdriver.Chrome(executable_path=r'C:/Users/jguis/Downloads/chromedriver/chromedriver.exe')
+# browser = webdriver.Chrome(executable_path=r'C:/Users/jguis/Downloads/chromedriver/chromedriver.exe')
+browser = webdriver.Chrome(
+    executable_path=r'C:/Users/admin/OneDrive/chromedriver/chromedriver.exe')
 
 browser.get("https://github.com/TheDancerCodes")
 
@@ -16,7 +18,9 @@ browser.get("https://github.com/TheDancerCodes")
 timeout = 20
 
 try:
-    WebDriverWait(browser, timeout).until(EC.visibility_of_element_located((By.XPATH, "//img[@class='avatar width-full rounded-2']")))
+    WebDriverWait(browser, timeout).until(
+        EC.visibility_of_element_located(
+            (By.XPATH, "//img[@class='avatar width-full rounded-2']")))
 except TimeoutException:
     print("Timed out waiting for page to load")
     browser.quit()
@@ -29,8 +33,8 @@ titles = [x.text for x in titles_element]
 print('titles:')
 print(titles, '\n')
 
-
-language_element = browser.find_elements_by_xpath("//p[@class='mb-0 f6 text-gray']")
+language_element = browser.find_elements_by_xpath(
+    "//p[@class='mb-0 f6 text-gray']")
 # same concept as for list-comprehension above.
 languages = [x.text for x in language_element]
 print("languages:")
@@ -39,6 +43,6 @@ print(languages, '\n')
 for title, language in zip(titles, languages):
     print("RepoName : Language")
     print(title + ": " + language, '\n')
-    
+
 time.sleep(15)
 browser.quit()
