545673a99547a9bc0ec71320344d5050ee3b8bfc
Jose Guisao <jguisao@gmail.com>
2022-01-25 18:02:19 -0400

    update code

--- webscrape/getImagesUrls-2-celebreties.py
+++ webscrape/getImagesUrls-2-celebreties.py
@@ -1,54 +1,51 @@
 #/usr/bin/python3
-import requests
+import requests, re, time
 from bs4 import BeautifulSoup as BSoup
 from selenium import webdriver
-import re
 
 chromedriver = 'C:/Users/admin/OneDrive/chromedriver/chromedriver.exe'
-driver = webdriver.Chrome(chromedriver)
-url  = 'https://stacker.com/stories/1173/celebrities-you-didnt-know-were-lgbtq'
+# driver = webdriver.Chrome(chromedriver)
+url = 'https://stacker.com/stories/1173/celebrities-you-didnt-know-were-lgbtq'
 url3 = 'https://kiwireport.com/celebrity-children-who-look-just-like-their-parents-did-at-the-same-age'
 url4 = 'https://herbeauty.co/en/foodtravel/best-places-to-travel-in-2021'
-url5 = 'https://investingfuel.com/entertainment/27-stars-from-the-90s-where-are-they-now/29?utm_source=if_end&utm_campaign=gft_1&utm_medium=cpc&utm_term='
-url1 = 'https://tenor.com/search/cat-gifs' # cats gifs
-url2 = 'https://tenor.com/search/dog-gifs' # dogs gifs
-bs_obj = BSoup(driver.page_source, 'html.parser')
+url5 = 'https://investingfuel.com/entertainment/27-stars-from-the-90s-where-are-they-now'
+url5_1 = 'https://investingfuel.com/entertainment/27-stars-from-the-90s-where-are-they-now/155'
+url1 = 'https://tenor.com/search/cat-gifs'  # cats gifs
+url2 = 'https://tenor.com/search/dog-gifs'  # dogs gifs
+# driver.get(url5)
+# time.sleep(3)
+# driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+time.sleep(4)
+# bs_obj = BSoup(driver.page_source, 'html.parser')
 response = requests.get(url5)
+time.sleep(3)
 bs_obj = BSoup(response.text, 'html.parser')
-driver.close()
-images = bs_obj.findAll('img')
-i=0
+images1 = bs_obj.findAll('img')
+print(len(images1))
+# driver.get(url5_1)
+# time.sleep(3)
+# driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+# time.sleep(4)
+# bs_obj = BSoup(driver.page_source, 'html.parser')
+response = requests.get(url5_1)
+time.sleep(3)
+bs_obj = BSoup(response.text, 'html.parser')
+
+images2 = bs_obj.findAll('img')
+print(len(images2))
 #Usar el title de la pagina para crear el filename, pero eliminando todo especial characters
 fname = bs_obj.title
-# removing special characters from a list of items in python
-fname = ([re.sub('[^a-zA-Z0-9]+', '', _ ) for _ in fname])
 print(fname)
-f = open(''.join(fname),"w")
 
-for image in images:
-	i = i + 1
-	# lnk = re.search('^http.*gif', image['src'] )
-	# print(type(lnk.group(0)))
-	f.write(image['src']+'\n')
-	# print(i,image['src'])
-print(len(images))
-
-f.close()
-
-f = open(''.join(fname),'r')
-lines = f.readlines()
-# regex = r"^http.*.gif"
-regex = r"^http.*.png"
-# https://regex101.com/r/evkgYU/1/
-# https://regex101.com/r/evkgYU/1/codegen?language=python
-
-# las Lines que siguen despues del for loop son de este site, tiene un code generator
-
-for line in lines:
-	matches = re.finditer(regex,line)
-	for matchNum, match in enumerate(matches, start=1):
-		print("{match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()))
-		for groupNum in range(0, len(match.groups())):
-			groupNum = groupNum + 1
-			print("Group {groupNum} found at {start}-{end}: {group}".format(groupNum = groupNum, start = match.start(groupNum), end = match.end(groupNum), group = match.group(groupNum)))
-f.close()
+# # removing special characters from a list of items in python
+# fname = ([re.sub('[^a-zA-Z0-9]+', '', _) for _ in fname])
+# print(fname)
+# f = open(''.join(fname), "w")
+i = 0
+for image in images1 + images2:
+    i = i + 1
+    # lnk = re.search('^http.*gif', image['src'] )
+    # print(type(lnk.group(0)))
+    # f.write(image['src'] + '\n')
+    print(i, image['src'])
+print(len(images2))
