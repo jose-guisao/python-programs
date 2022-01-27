8aaee1851a7e95922284f3a7467a08cc41e0f490
Jose Guisao <jguisao@gmail.com>
2022-01-27 17:27:36 -0400

    correct images path

--- get_images_urls_from_website.py
+++ get_images_urls_from_website.py
@@ -17,7 +17,7 @@ def download_file(url):
     local_fname = url.split('/')[-1]
     with requests.get(url, stream = True) as r:
         r.raise_for_status()
-        with open("c:/Users/admin/OneDrive/Documents/python-programs/"+local_fname, 'wb') as f:
+        with open("c:/Users/admin/OneDrive/Documents/python-programs/images/"+local_fname, 'wb') as f:
                   for chunk in r.iter_content(chunk_size=8192):
                       f.write(chunk)
     return local_fname
