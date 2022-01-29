776b5e284251c4fff3ae37205e3e882766566d0f
Jose Guisao <jguisao@gmail.com>
2022-01-29 03:48:38 -0400

    add sqlite functionality

--- date_convert_str.py
+++ date_convert_str.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 from bs4 import BeautifulSoup as BSoup
 import requests
+import sqlite3
 import time
 url = 'https://townhall.com/political-cartoons'
 url1 = 'https://townhall.com/political-cartoons/2022/01/27'
-day = input('Enter day number:')
+day = input('Enter day number: ')
 link_dt = '/2022/01/'
 for d in range(1, int(day)):
     url_dt = url + link_dt + str(d)
@@ -17,3 +18,18 @@ for d in range(1, int(day)):
           ['src'], ' ', toon_date)
     print(bs_obj.find('h4', class_='cartoon-author').a.text)
 print('End program')
+
+db = sqlite3.connect(db_name)
+cursor = db.cursor()
+cursor.executescript('drop table if exists img_links;')
+cursor.execute('''
+    CREATE TABLE toon_img_links(id INTEGER PRIMARY KEY, link TEXT, name TEXT, content TEXT)
+''')
+db.commit()
+
+for i in range(0, len(toons_lnks)):
+    cursor.execute('''INSERT INTO toon_img_links (id, link, name, content) VALUES (?,?,?,?)''',
+                   (i+1, toons_lnks[i], authors[i], dates[i]))
+print("End second part ... saving database")
+db.commit()
+db.close()
--- get_images_urls_from_townhall_selenium.py
+++ get_images_urls_from_townhall_selenium.py
@@ -5,14 +5,17 @@
 from bs4 import BeautifulSoup as BSoup
 ##from urllib.request import Request, urlopen
 from selenium import webdriver
-import requests, datetime, sqlite3, re, time
+import requests
+import datetime
+import sqlite3
+import re
+import time
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import ElementNotVisibleException
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
 #ser = Service("C:\\chromedriver.exe")
-
 
 
 urls = [
@@ -26,7 +29,7 @@ urls = [
     'https://stacker.com/stories/2189/50-best-colleges-east-coast',
     'https://stacker.com/stories/145/top-100-country-songs-all-time',
     'https://stacker.com/stories/980/100-best-tv-shows-all-time']
-#https://townhall.com/political-cartoons/patcross/2022/01/12/187713
+# https://townhall.com/political-cartoons/patcross/2022/01/12/187713
 url = 'https://townhall.com/political-cartoons'
 
 ser = Service('C:/Users/admin/OneDrive/chromedriver/chromedriver.exe')
@@ -35,6 +38,7 @@ s = webdriver.Chrome(service=ser, options=op)
 
 ##driver = webdriver.Chrome(chromedriver)
 s.get(url)
+
 
 def download_file(url):
     local_fname = url.split('/')[-1]
@@ -45,6 +49,7 @@ def download_file(url):
                 f.write(chunk)
     return local_fname
 
+
 response = requests.get(url)
 bs_obj = BSoup(s.page_source, 'html.parser')
 fname_stamp = datetime.datetime.now().strftime("_%d%b%Y-%H%M")
@@ -53,8 +58,10 @@ fname = bs_obj.title
 # # removing special characters from a list of items in python
 fname = ([re.sub('[^a-zA-Z]+', '', _) for _ in fname])
 print(fname, fname_stamp)
-f = open("c:/Users/admin/OneDrive/Documents/python-programs/images/"+''.join(fname)+fname_stamp, "w")
-db_name = "c:/Users/admin/OneDrive/Documents/python-programs/images/" + ''.join(fname) + '.db'
+f = open("c:/Users/admin/OneDrive/Documents/python-programs/images/" +
+         ''.join(fname)+fname_stamp, "w")
+db_name = "c:/Users/admin/OneDrive/Documents/python-programs/images/" + \
+    ''.join(fname) + '.db'
 
 toons_lnks = []  # list to save links
 authors = []
@@ -63,33 +70,36 @@ dates = []
 chk_date = True
 
 while chk_date == True:
-        f.write(bs_obj.find('img',class_='cartoon-image')['src']+'\n')
-        author = bs_obj.find('h4',class_='cartoon-author').a.text
-        btn_forward = s.find_element(By.XPATH, "//*[@id='slideshow-container']/div[1]/div[1]/div[1]/a/i").click()
-        bs_obj = BSoup(s.page_source, 'html.parser')
-        time.sleep(1)
-        toon_date = s.find_element(By.XPATH, "//*[@id='slideshow-container']/div[1]/div[2]/h4/span")
-        toons_lnks.append(bs_obj.find('img',class_='cartoon-image')['src']+'\n')
-        authors.append(author)
-        dates.append(toon_date.text)
-        print(toon_date.text)
-        print(bs_obj.find('img',class_='cartoon-image')['src'])
-        if 'November 24' in s.find_element(By.XPATH, "//*[@id='slideshow-container']/div[1]/div[2]/h4/span").text:
-        	chk_date = False
-        	print('Reach end')
-        else:
-        	chk_date = True
+    f.write(bs_obj.find('img', class_='cartoon-image')['src']+'\n')
+    author = bs_obj.find('h4', class_='cartoon-author').a.text
+    btn_forward = s.find_element(
+        By.XPATH, "//*[@id='slideshow-container']/div[1]/div[1]/div[1]/a/i").click()
+    bs_obj = BSoup(s.page_source, 'html.parser')
+    time.sleep(1)
+    toon_date = s.find_element(
+        By.XPATH, "//*[@id='slideshow-container']/div[1]/div[2]/h4/span")
+    toons_lnks.append(bs_obj.find('img', class_='cartoon-image')['src']+'\n')
+    authors.append(author)
+    dates.append(toon_date.text)
+    print(toon_date.text)
+    print(bs_obj.find('img', class_='cartoon-image')['src'])
+    if 'November 24' in s.find_element(By.XPATH, "//*[@id='slideshow-container']/div[1]/div[2]/h4/span").text:
+        chk_date = False
+        print('Reach end')
+    else:
+        chk_date = True
 f.close()
 db = sqlite3.connect(db_name)
 cursor = db.cursor()
 cursor.executescript('drop table if exists img_links;')
 cursor.execute('''
-    CREATE TABLE img_links(id INTEGER PRIMARY KEY, link TEXT, name TEXT, content TEXT)
+    CREATE TABLE toon_img_links(id INTEGER PRIMARY KEY, link TEXT, name TEXT, content TEXT)
 ''')
 db.commit()
 
 for i in range(0, len(toons_lnks)):
-    cursor.execute('''INSERT INTO img_links (id, link, name, content) VALUES (?,?,?,?)''', (i+1, toons_lnks[i], authors[i], dates[i]))
+    cursor.execute('''INSERT INTO toon_img_links (id, link, name, content) VALUES (?,?,?,?)''',
+                   (i+1, toons_lnks[i], authors[i], dates[i]))
 print("End second part ... saving database")
 db.commit()
 db.close()
