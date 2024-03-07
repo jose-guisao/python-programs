0e6f42bd24533d302b341c0f75fe6b0378728844
jose-guisao <74114153+jose-guisao@users.noreply.github.com>
2024-03-07 00:21:30 -0400

    add positive cristianos

--- webscrape-selenium-find-elements-test.py
+++ webscrape-selenium-find-elements-test.py
@@ -19,7 +19,8 @@ os.chdir(datafilepath)
 query = input("Enter search entries separeted by space: ")
 url2='https://duckduckgo.com/?q=tech+magazine+covers&t=chromentp&iar=images&iax=images&ia=images'
 url1='https://duckduckgo.com/?t=h_&q=Positive+Quotes&iax=images&ia=images'
-driver.get(url1)
+url3='https://duckduckgo.com/?q=mensajes+positivos+cristianos&t=h_&iax=images&ia=images'
+driver.get(url3)
 time.sleep(10)
 driver.implicitly_wait(0.5)
 html = driver.page_source
